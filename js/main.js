bc38b27bdb8456a943a6583494d368d47b3979dd
Kafi Rashid <kafi.rashid@gmail.com>
2018-03-20 11:50:31 +0600

    Init

--- js/main.js
+++ js/main.js
@@ -5,15 +5,15 @@ jQuery(document).ready(function(){
 		$('.star').remove();
 		var vp_w = stars.width();
 		var vp_h = stars.height();
-		for (var i = 0; i < 200; i++) {
+		for (var i = 0; i < 300; i++) {
 			stars.append("<div class='star' style='left: "+Math.floor((Math.random() * vp_w) + 1)+"px; top: "+Math.floor((Math.random() * vp_h) + 1)+"px'></div>");
 		}
 		setInterval(function() {
-			for(var i = 0; i < 200; i++) {
-	            $('.star:nth-of-type('+Math.floor(Math.random() * (i - i + 1)) + i+')').animate({
-	                'opacity': Math.random() * (1 - 0) - (1 * 0.3)
-	            }, 1000);
+			for(var i = 0; i < 300; i++) {
+	            $('.star:nth-of-type('+Math.floor(Math.random() * 1) + i+')').animate({
+	                'opacity': Math.random() * 1 - (1 * 0.1)
+	            }, 500);
 	        }
-		}, 1000)
+		}, 750)
 	});
 });
